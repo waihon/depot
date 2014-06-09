f6ff69b0ec591be3aec3e24f15d9926f09fcd2e0
Waihon Yew <yewwaihon@gmail.com>
2014-06-09 20:01:40 +0800

    A Smarter Cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,8 @@
 class LineItem < ActiveRecord::Base
   belongs_to :product # LineItem references Product => Product has many Line Items
   belongs_to :cart    # Cart has many Line Items
+
+  def total_price
+    product.price * quantity
+  end
 end
